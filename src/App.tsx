47d947d793514f9c5d266f14e90d70a338a3b7d7
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-06-16 15:53:47 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FaFacebook, FaGoogle, FaApple } from "react-icons/fa";
+import SNSLogin from "./SNSLogin";
+
+function App() {
+  return (
+    <div className="App">
+      <SNSLogin backgroundColor="#1877F2" snsText={{ sns: "Facebook" }} snsIcon={{ Icon: FaFacebook }} />
+      <SNSLogin backgroundColor="black" snsText={{ sns: "Apple" }} snsIcon={{ Icon: FaApple }} />
+      <SNSLogin backgroundColor="white" snsText={{ sns: "Google" }} snsIcon={{ Icon: FaGoogle }} />
+    </div>
+  );
+}
+
+export default App;
